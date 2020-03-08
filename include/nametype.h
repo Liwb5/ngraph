987c31030ce8b41ec739ef10799ed1469ba33e4b
liweibin02 <liweibin02@baidu.com>
2020-03-08 10:02:33 +0800

    move all typedef to a file for convenient

---
+++ include/nametype.h
@@ -0,0 +1,13 @@
+#ifndef __NAMETYPE_H
+#define __NAMETYPE_H
+
+#include <vector>
+#include <stdint.h>
+
+using namespace std;
+
+typedef uint64_t vertex_t;
+typedef vector<vertex_t> IdArray;
+typedef vector<vector<vertex_t>> d2Array;
+
+#endif /*__NAMETYPE_H*/
